2ec911f79a826ad4a6ff740dc4e8de6f9d36b218
sga02 <stephen_ashley@homedepot.com>
2020-12-14 15:42:03 -0500

    Initial push for branch DirectivesAndViews

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 (function() {
 
-    var app = angular.module("githubViewer", []);
+  var app = angular.module("githubViewer", []);
 
-    var MainController = function($scope, $http) {
+  var MainController = function($scope, $http) {
 
-        $scope.message = "Hello, Angular!";
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+    };
 
-        var onUserComplete = function(response) {
-            $scope.user = response.data;
-        };
-
-        var onError = function(reason) {
-            $scope.error = "Could not fetch the user";
-        };
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the user: " + reason.error;
+    };
 
 
         $http.get("https://api.github.com/users/sashle63")
-            .then(onUserComplete, onError)
+      .then(onUserComplete, onError);
 
-        $scope.message = "Hello, Angular!";
-    };
 
-    app.controller("MainController", ["$scope", "$http", MainController]);
+    $scope.message = "Hello, Angular!";
+
+
+  };
+
+  app.controller("MainController", ["$scope", "$http", MainController]);
 
 }());
