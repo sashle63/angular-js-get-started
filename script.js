09195dbcb43cd203f3790351f549d575dbc76e6c
sga02 <stephen_ashley@homedepot.com>
2020-12-14 14:48:25 -0500

    Initial push for branch Controllers

---
+++ script.js
@@ -0,0 +1,26 @@
+(function() {
+
+    var app = angular.module("githubViewer", []);
+
+    var MainController = function($scope, $http) {
+
+        $scope.message = "Hello, Angular!";
+
+        var onUserComplete = function(response) {
+            $scope.user = response.data;
+        };
+
+        var onError = function(reason) {
+            $scope.error = "Could not fetch the user";
+        };
+
+
+        $http.get("https://api.github.com/users/sashle63")
+            .then(onUserComplete, onError)
+
+        $scope.message = "Hello, Angular!";
+    };
+
+    app.controller("MainController", ["$scope", "$http", MainController]);
+
+}());
